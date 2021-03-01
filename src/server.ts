f55698a874b0c4f821742ed35576d14e41a99f5a
hlfdev <pedrohlf.contato@gmail.com>
2021-03-01 00:17:55 -0300

    NLW04-API-02

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,10 @@
+import 'reflect-metadata';
 import express from 'express';
-
+import './database';
+import { router } from './routes';
 const app = express();
 
-// GET - SEARCH METHOD
-
-// POST - SAVE METHOD
-
-// PUT - CHANGE METHOD
-
-// DELETE - DELETE METHOD
-
-// PATCH - CHANGE SPECIFIC METHOD
-
-// http://localhost:3333/
-
-app.get("/", (request, response) => {
-    return response.json({
-        message:"Hello, World - NLW#04"
-    });
-})
-
-app.post("/", (request, response) => {
-    return response.json({
-        message:"Os dados foram salvos com Sucesso!"
-    });
-})
-
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
