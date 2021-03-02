a197d740456d0e8821cfc8fec07eef1971130731
hlfdev <pedrohlf.contato@gmail.com>
2021-03-02 00:51:40 -0300

    NLW04-API-03

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import express from 'express';
+import createConnection from './database';
+import { router } from './routes';
+const app = express();
+
+
+createConnection();
+
+app.use(express.json());
+app.use(router);
+
+
+export { app };
